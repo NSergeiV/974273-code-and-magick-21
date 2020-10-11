0d02baf06e1d80afdcf984d6ba07ed24949abf7a
NSergeiV <ttniseva@yandex.ru>
2020-10-11 21:14:26 +0300

    Выполнил - 9. Учебный проект: одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,12 @@
 'use strict';
+
+// Вывод основного персанажа и участников на Попап
+
 let setupBlock = document.querySelector('.setup');
 let setupSimilar = document.querySelector('.setup-similar');
-setupBlock.classList.remove('hidden');
-setupSimilar.classList.remove('hidden');
+
+// setupBlock.classList.remove('hidden');
+// setupSimilar.classList.remove('hidden');
 
 const NAMES_WIZARDS = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 const SURNAMES_WIZARDS = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
@@ -38,3 +42,83 @@ let creatingGroupWizards = function (array) {
 };
 
 setupSimilarList.appendChild(creatingGroupWizards(wizards));
+
+// Конец Вывода основного персанажа и участников на Попап
+
+// Открытие и закрытие Попап основного персанажа
+
+const KEY_ENTER = 13;
+const KEY_ESC = 27;
+let setupOpen = document.querySelector('.setup-open');
+let setupClose = document.querySelector('.setup-close');
+let setupUserName = setupBlock.querySelector('.setup-user-name');
+
+let onPopupEscPress = function (evt) {
+  if (setupUserName !== document.activeElement) {
+    if (evt.keyCode === KEY_ESC) {
+      evt.preventDefault();
+      closePopap();
+    }
+  }
+  if (setupClose === document.activeElement) {
+    if (evt.keyCode === KEY_ENTER) {
+      evt.preventDefault();
+      closePopap();
+    }
+  }
+  if (setupUserName === document.activeElement) {
+    if (evt.keyCode === KEY_ENTER) {
+      evt.preventDefault();
+      openPopap();
+    }
+  }
+};
+
+let onPopupClickPress = function () {
+  closePopap();
+};
+
+let closePopap = function () {
+  setupBlock.classList.add('hidden');
+
+  document.removeEventListener('keydown', onPopupEscPress);
+  setupClose.removeEventListener('click', onPopupClickPress);
+};
+
+let openPopap = function () {
+  setupBlock.classList.remove('hidden');
+
+  document.addEventListener('keydown', onPopupEscPress);
+  setupClose.addEventListener('click', onPopupClickPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopap();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === KEY_ENTER) {
+    evt.preventDefault();
+    openPopap();
+  }
+});
+
+// Меняем цвет: мантии, глаз, фаурбола. Записываем в скрытые поля.
+
+const FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+let setupWizard = document.querySelector('.setup-wizard');
+let wizardEyes = setupWizard.querySelector('.wizard-eyes');
+let wizardCoat = setupWizard.querySelector('.wizard-coat');
+let setupFireballWrap = document.querySelector('.setup-fireball-wrap');
+
+wizardCoat.addEventListener('click', function () {
+  document.querySelector('input[name="coat-color"]').value = wizardCoat.style.fill = COATS_WIZARDS[randomNamber(COATS_WIZARDS)];
+});
+
+wizardEyes.addEventListener('click', function () {
+  document.querySelector('input[name="eyes-color"]').value = wizardEyes.style.fill = EYES_WIZARDS[randomNamber(EYES_WIZARDS)];
+});
+
+setupFireballWrap.addEventListener('click', function () {
+  document.querySelector('input[name="fireball-color"]').value = setupFireballWrap.style.backgroundColor = FIREBALL_COLORS[randomNamber(FIREBALL_COLORS)];
+});
